4044e98c9f6e0b3947258b73fa1f69c248ea5ec9
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2023-11-10 13:36:05 -0300

    Add functions to interact with metrics

--- src/Metrics.hs
+++ src/Metrics.hs
@@ -1,12 +1,16 @@
 module Metrics
   ( AppMetrics,
-    metrics,
+    newMetrics,
     printMetrics,
   )
 where
 
+import Data.Foldable (for_)
 import Data.IORef
 import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
+import Data.Time.Clock (diffUTCTime, getCurrentTime, nominalDiffTimeToSeconds)
+import Text.Printf (printf)
 
 data AppMetrics = AppMetrics
   { successCount :: Int,
@@ -14,6 +18,8 @@ data AppMetrics = AppMetrics
     callDuration :: Map.Map String Int
   }
   deriving (Eq, Show)
+
+newtype Metrics = Metrics {appMetricsStore :: IORef AppMetrics}
 
 metrics :: IO (IORef AppMetrics)
 metrics =
@@ -24,5 +30,46 @@ metrics =
         callDuration = Map.empty
       }
 
+newMetrics :: IO Metrics
+newMetrics = Metrics <$> metrics
+
 printMetrics :: IO ()
 printMetrics = metrics >>= readIORef >>= print
+
+tickSuccess :: Metrics -> IO ()
+tickSuccess (Metrics metricsRef) =
+  modifyIORef metricsRef $ \m ->
+    m {successCount = 1 + m.successCount}
+
+tickFailure :: Metrics -> IO ()
+tickFailure (Metrics metricsRef) = modifyIORef metricsRef $ \m ->
+  m {failureCount = 1 + m.failureCount}
+
+timeFunction :: Metrics -> String -> IO a -> IO a
+timeFunction (Metrics metricsRef) actionName action = do
+  startTime <- getCurrentTime
+  result <- action
+  endTime <- getCurrentTime
+  modifyIORef metricsRef $ \oldMetrics ->
+    let oldDurationValue =
+          fromMaybe 0 $ Map.lookup actionName oldMetrics.callDuration
+        runDuration =
+          floor . nominalDiffTimeToSeconds $
+            diffUTCTime endTime startTime
+        newDurationValue = oldDurationValue + runDuration
+     in oldMetrics
+          { callDuration =
+              Map.insert
+                actionName
+                newDurationValue
+                oldMetrics.callDuration
+          }
+  pure result
+
+displayMetrics :: Metrics -> IO ()
+displayMetrics (Metrics metricsStore) = do
+  appMetrics <- readIORef metricsStore
+  putStrLn $ "successes:" <> show appMetrics.successCount
+  putStrLn $ "failures:" <> show appMetrics.failureCount
+  for_ (Map.toList appMetrics.callDuration) $ \(functionName, timing) ->
+    putStrLn $ printf "Time spent in \"%s\": %d" functionName timing
