6d025185f1a6e8ae009a4ff3c9dd080ffaba698b
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-01-03 11:45:10 -0300

    Suggest words based on distance

--- src/NaiveSpellchecker.hs
+++ src/NaiveSpellchecker.hs
@@ -2,6 +2,7 @@ module NaiveSpeelChecker where
 
 import Data.Text (Text)
 import qualified Data.Text as T
+import Text.Printf
 
 editDistance :: Text -> Text -> Int
 editDistance stringA stringB
@@ -15,3 +16,27 @@ editDistance stringA stringB
     deleteCost = editDistance restOfA stringB
     insertCost = editDistance stringA restOfB
     swapCost = editDistance restOfA restOfB
+
+data SuggestedMatch = SuggestedMatch
+  { matchWord :: Text,
+    matchSearchedWord :: Text,
+    matchDistance :: Int
+  }
+  deriving (Eq, Show)
+
+showSuggestedMatch :: SuggestedMatch -> String
+showSuggestedMatch s =
+  printf "%s -> %s: %d" s.matchWord s.matchSearchedWord s.matchDistance
+
+spellCheckWord :: [Text] -> Int -> Text -> [SuggestedMatch]
+spellCheckWord dictionary threshold word =
+  getSuggestions dictionary []
+  where
+    getSuggestions [] suggestions = suggestions
+    getSuggestions (dictWord:dict) suggestions
+        | distance == 0 = []
+        | distance > threshold = getSuggestions dict suggestions
+        | otherwise = getSuggestions dict (suggestion : suggestions)
+        where
+          distance = editDistance dictWord word
+          suggestion = SuggestedMatch dictWord word distance
